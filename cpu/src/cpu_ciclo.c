0fe56c55024a153da627ab436378394f0593dfe7
JuanFernandezV <jfernandezvazquez@frba.utn.edu.ar>
2026-06-18 20:21:55 -0300

    se agregan todo para el merge

--- cpu/src/cpu_ciclo.c
+++ cpu/src/cpu_ciclo.c
@@ -76,6 +76,17 @@ static bool ejecutar_syscall(
         case CPU_INST_EXIT:
             return instruccion->cantidad_parametros == 0 &&
                    enviar_syscall_exit(socket_kernel, pid, logger);
+        case CPU_INST_MEM_ALLOC:
+
+            if(instruccion->cantidad_parametros != 2) {
+                return false;
+            }
+
+            return enviar_syscall_mem_alloc(socket_kernel, pid, parsear_uint32(instruccion->parametros[0]),
+                parsear_uint32(instruccion->parametros[1]), logger);
+        case CPU_INST_MEM_FREE:
+            return instruccion->cantidad_parametros == 1 &&
+                   enviar_syscall_mem_free(socket_kernel, pid, parsear_uint32(instruccion->parametros[0]), logger);
         default:
             return false;
     }
@@ -88,7 +99,9 @@ static bool es_syscall_o_exit(t_opcode_cpu opcode) {
            opcode == CPU_INST_SLEEP ||
            opcode == CPU_INST_STDOUT ||
            opcode == CPU_INST_STDIN ||
-           opcode == CPU_INST_EXIT;
+           opcode == CPU_INST_EXIT ||
+           opcode == CPU_INST_MEM_ALLOC ||
+           opcode == CPU_INST_MEM_FREE;
 }
 
 t_resultado_ciclo_cpu ejecutar_ciclo_proceso(
